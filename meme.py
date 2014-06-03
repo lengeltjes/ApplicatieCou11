e02b96d82eee2515ccce1484df66fba201b6166c
Leonoor Engeltjes <tndoo@hlo-ws131304.(none)>
2014-06-03 10:27:31 +0200

    MEME module + superscript

---
+++ meme.py
@@ -0,0 +1,13 @@
+import os
+import subprocess
+
+work_dir = "~/owex_pgx/meme/"
+input_files = os.listdir(work_dir)
+
+for f in input_files:
+	if ".fa" not in f or "tss" in f: #grab all fasta files that were not an input file earlier on
+		input_files.remove(f)
+	os.chdir("/sharing/students/MEME/bin")
+	subdir = f.split(".fa")[0] #begin filename = naam subfolder, i.e. NC1_output
+	command = "meme "+work_dir+f+" -oc "+work_dir+subdir+" -dna -mod zoops -minw 35 -maxw 45"
+	p = subprocess.call(command, shell=TRUE) #runt bovenstaand command in de shell
--- superscript.R
+++ superscript.R
@@ -1,3 +1,3 @@
-system("Rscript inlezen2.R")
+system("Rscript inlezen.R")
 system("Rscript display.R")
 system("Rscript filter.R")
