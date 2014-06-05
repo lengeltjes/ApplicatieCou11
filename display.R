a467d8322c225ad56debc7e637364a16c9960bf2
Leonoor Engeltjes <tndoo@hlo-ws131316.(none)>
2014-06-05 15:15:45 +0200

    werkende MEME.py + as gewijzigd

--- display.R
+++ display.R
@@ -1,40 +1,40 @@
 ratio <- scan("ratio.txt")
 
 png(filename= "scatterplot1.png")
-plot(ratio,main= "Scatterplot before filtering", ylab= "log2 ratio", xlab="tag")
+plot(ratio,main= "Scatterplot before filtering", ylab= "RPKM", xlab="tag")
 dev.off()
 
 png(filename= "boxplot1.png")
-boxplot(ratio, main= "Boxplot before filtering", ylab= "log2 ratio", xlab="tag")
+boxplot(ratio, main= "Boxplot before filtering", ylab= "RPKM", xlab="tag")
 dev.off()
 
 vec1 <- scan("vec1.txt")
 
 png(filename= "scatterplot2.png")
-plot(vec1,main= "Scatterplot after filtering", ylab= "log2 ratio", xlab="tag")
+plot(vec1,main= "Scatterplot after filtering", ylab= "RPKM", xlab="tag")
 dev.off()
 
 png(filename= "boxplot2.png")
-boxplot(vec1, main= "Boxplot after filtering", ylab= "log2 ratio", xlab="tag")
+boxplot(vec1, main= "Boxplot after filtering", ylab= "RPKM", xlab="tag")
 dev.off()
 
 vec4 <- scan("vec4.txt")
 
 png(filename= "scatterplot3.png")
-plot(vec4,main= "Scatterplot after filtering (without FDR filtering)", ylab= "log2 ratio", xlab="tag")
+plot(vec4,main= "Scatterplot after filtering (without FDR filtering)", ylab= "RPKM", xlab="tag")
 dev.off()
 
 png(filename= "boxplot3.png")
-boxplot(vec4, main= "Boxplot after filtering (without FDR filtering)", ylab= "log2 ratio", xlab="tag")
+boxplot(vec4, main= "Boxplot after filtering (without FDR filtering)", ylab= "RPKM", xlab="tag")
 dev.off()
 
 vec6 <- scan("vec6.txt")
 
 png(filename= "scatterplot4.png")
-plot(vec6,main= "Scatterplot after filtering (without ratio filtering)", ylab= "log2 ratio", xlab="tag")
+plot(vec6,main= "Scatterplot after filtering (without ratio filtering)", ylab= "RPKM", xlab="tag")
 dev.off()
 
 png(filename= "boxplot4.png")
-boxplot(vec6, main= "Boxplot after filtering (without ratio filtering)", ylab= "log2 ratio", xlab="tag")
+boxplot(vec6, main= "Boxplot after filtering (without ratio filtering)", ylab= "RPKM", xlab="tag")
 dev.off()
 
--- meme.py
+++ meme.py
@@ -1,13 +1,24 @@
 import os
 import subprocess
 
-work_dir = os.path.expanduser("~/ApplicatieCou11/meme/")
+work_dir = os.path.expanduser("~/ApplicatieCou11/")
 input_files = os.listdir(work_dir)
+print(input_files)
 
 for f in input_files:
-	if ".fa" not in f or "tss" in f: #grab all fasta files that were not an input file earlier on
+	if ".fa" not in f: #grab all fasta files
+		print("Removing file "+f)
 		input_files.remove(f)
-	os.chdir("/sharing/students/MEME/bin")
-	subdir = f.split(".fa")[0] #begin filename = naam subfolder, i.e. NC1_output
-	command = "meme "+work_dir+f+" -oc "+work_dir+subdir+" -dna -mod zoops -minw 35 -maxw 45"
-	p = subprocess.call(command, shell=TRUE) #runt bovenstaand command in de shell
+		continue
+	elif "tss" in f:
+		print("Removing file "+f)
+		input_files.remove(f)
+		continue
+	else:
+		print("I kept file "+f)
+		subdir = f.split(".fa")[0] #begin filename = naam subfolder, i.e. NC1_output
+		command1 = "cd /sharing/students/meme_4.9.1/bin/"
+		command2 = "./meme "+work_dir+f+" -oc "+work_dir+subdir+" -dna -mod zoops -minw 35 -maxw 45"
+		print(command2)
+		p = subprocess.call(command1+" ; pwd ; "+command2, shell=True) #runt bovenstaand commands in de shell
+	# checkt tussendoor of ie in de goede map zit
